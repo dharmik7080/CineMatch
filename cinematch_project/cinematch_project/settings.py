ffea5a12bf229e69ea59ec4c1634b34813b0739b
Dharmik Thakkar <dharmikthakkar@Dharmiks-Macbook.local>
2026-07-03 21:50:25 +0530

    Checkpoint: Working baseline before implementing internal movie details view

--- cinematch_project/cinematch_project/settings.py
+++ cinematch_project/cinematch_project/settings.py
@@ -111,17 +111,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
+# 🌌 Paste this configuration right here:
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 # Custom Authentication URL redirection rules (Unit 9)
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
