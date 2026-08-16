889a6650863d410ff6dfe841060ffa697f50c7c2
Dharmik Thakkar <dharmikthakkar2203@gmail.com>
2026-08-16 14:41:14 +0530

    fix: change reCAPTCHA widget to ReCaptchaV3 to match the registered key type

--- core/forms.py
+++ core/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV3
 
 class CineMatchRegistrationForm(UserCreationForm):
-    captcha = ReCaptchaField()
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
 
 class CineMatchLoginForm(AuthenticationForm):
-    captcha = ReCaptchaField()
+    captcha = ReCaptchaField(widget=ReCaptchaV3)
