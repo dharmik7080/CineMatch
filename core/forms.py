2b4b0237eeaef0083ace315c79ddfb9d1d540faa
Dharmik Thakkar <dharmikthakkar2203@gmail.com>
2026-08-16 14:51:35 +0530

    feat: use new reCAPTCHA v2 Checkbox keys and update forms to ReCaptchaV2Checkbox widget

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django_recaptcha.fields import ReCaptchaField
-from django_recaptcha.widgets import ReCaptchaV2Invisible
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 
 class CineMatchRegistrationForm(UserCreationForm):
-    captcha = ReCaptchaField(widget=ReCaptchaV2Invisible)
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
 
 class CineMatchLoginForm(AuthenticationForm):
-    captcha = ReCaptchaField(widget=ReCaptchaV2Invisible)
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
