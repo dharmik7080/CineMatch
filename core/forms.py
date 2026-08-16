ab8be927115f2cfcd644b31dfb25d3908f700a41
Dharmik Thakkar <dharmikthakkar2203@gmail.com>
2026-08-16 14:41:50 +0530

    fix: change reCAPTCHA widget to ReCaptchaV2Invisible to match user v2 Invisible registration

--- core/forms.py
+++ core/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django_recaptcha.fields import ReCaptchaField
-from django_recaptcha.widgets import ReCaptchaV3
+from django_recaptcha.widgets import ReCaptchaV2Invisible
 
 class CineMatchRegistrationForm(UserCreationForm):
-    captcha = ReCaptchaField(widget=ReCaptchaV3)
+    captcha = ReCaptchaField(widget=ReCaptchaV2Invisible)
 
 class CineMatchLoginForm(AuthenticationForm):
-    captcha = ReCaptchaField(widget=ReCaptchaV3)
+    captcha = ReCaptchaField(widget=ReCaptchaV2Invisible)
